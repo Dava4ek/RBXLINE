8310b9697b860994f1c3818114f12e55ad589caf
David <101060716+Dava4ek@users.noreply.github.com>
2022-12-23 21:07:05 +0300

    rbxline v3
    RBXLINE v3

--- rbx/assets/js/script.js
+++ rbx/assets/js/script.js
@@ -96,15 +96,31 @@ if (browserok) {
 /*==============================================================================*/
 /*                              Navbar                                          */
 /*==============================================================================*/
-const adaptivnae = document.getElementById("adaptivnae");
-const ul = document.querySelector(".navbar ul");
-
-adaptivnae.addEventListener("click", () => {
-  /* adaptivnae.classList.toggle("show-x"); */
-  ul.classList.toggle("show");
-});
-
+function toggleMobileMenu() {
+    document.querySelector('#menu').classList.toggle('active')
+    document.querySelector('.mobile-bar').classList.toggleMobileMenu('active')
+}
 /*==============================================================================*/
 /*                              FAQ                                             */
 /*==============================================================================*/
 
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
