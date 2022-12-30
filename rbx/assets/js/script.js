7705405b68f5aa62d9376e6cff0d66d469ebc744
David <101060716+Dava4ek@users.noreply.github.com>
2022-12-30 20:11:13 +0300

    RBXLINE v3

--- rbx/assets/js/script.js
+++ rbx/assets/js/script.js
@@ -88,10 +88,25 @@ if (browserok) {
 
 
 /*==============================================================================*/
-/*                              Register                                        */
+/*                              Tabcontent                                      */
 /*==============================================================================*/
+function openContent(contentName, elmnt, color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
 
-/* =) */
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+
+    document.getElementById(contentName).style.display = "block";
+
+}
+
+document.getElementById("defaultOpen").click();
 
 /*==============================================================================*/
 /*                              Navbar                                          */
@@ -104,23 +119,9 @@ function toggleMobileMenu() {
 /*                              FAQ                                             */
 /*==============================================================================*/
 
-function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+/*==============================================================================*/
+/*                              Button TOP                                      */
+/*==============================================================================*/
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
